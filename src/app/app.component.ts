d548284d96525f490676fd7e0b6827e676d78c8f
Lucky Hlakaza <nhlalucky9@gmail.com>
2021-01-17 00:03:14 +0200

    feat(cdk): added angular cdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Boxcommerce';
+  amount = 150;
+  randTousdRate = 16.23;
+  convertedValue: any = 0;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.convertedValue = this.calculateExchangeRate(this.randTousdRate, 1);
+  }
+
+  calculateExchangeRate(fromRate: number, toRate: number): string {
+    return ((this.amount * toRate) / fromRate).toFixed(5);
+  }
 }
